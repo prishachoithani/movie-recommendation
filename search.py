a788696975ba4567c1b568bdbbd4de53e98eaceb
prishachoithani <prishachoithani@gmail.com>
2025-11-25 23:56:34 +0530

    Update search.py

--- search.py
+++ search.py
@@ -6,7 +6,7 @@ from data import movies
 def search_movie():
    # Step 2: Ask the user for a movie name (or partial name)
     name = input("\nEnter movie name to search: ").strip().lower()
-    found = False # Boolean flag 
+    found = False # Boolean flag to
 
     print()
     for movie in movies:
