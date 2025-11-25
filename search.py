2aa6c03633ab5d0c093bce752d15986c5367efd1
prishachoithani <prishachoithani@gmail.com>
2025-11-26 00:08:16 +0530

    search.py
    Clarified comment to improve readability.

--- search.py
+++ search.py
@@ -11,7 +11,7 @@ def search_movie():
     print()
     for movie in movies:
         if name in movie.lower():
-           # If found, display
+           # If found, display the
             print(f"Found: {movie} ({movies[movie]})")
             found = True
 
